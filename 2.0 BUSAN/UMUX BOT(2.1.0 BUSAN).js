9b8e5f553f053c9cd14a9cc61ce7e77dc9a98902
HonestSquare <46321398+HonestSquare@users.noreply.github.com>
2020-10-25 19:14:27 +0900

    UMUX 2.1.0 r16
    다음과 같은 업데이트 사항이 있습니다.
    - 버그 수정
    - 최적화 개선

--- 2.0 BUSAN/UMUX BOT(2.1.0 BUSAN).js
+++ 2.0 BUSAN/UMUX BOT(2.1.0 BUSAN).js
@@ -1,4 +1,4 @@
-// API LEVEL(VERSION): 7(2.1.0 r15)
+// API LEVEL(VERSION): 7(2.1.0 r16)
 //==========================================<README>==========================================
 // 유즈맵 대표카페(이하 UM)에서 진행하고 있는 한국어화 유즈맵 봇방 프로젝트로, 
 // 사용자 인터페이스(UI)뿐만 아니라 플레이의 매사 모든 순간까지 아우르는 사용자 경험(UX)입니다.
@@ -345,13 +345,7 @@ class GameManager{
 		}
 		this.onKickRateLimitSet = function(						// 킥 제한 설정
 			min, rate, burst, player){
-				room.setKickRateLimit(min, rate, burst);
-				NC.announce(NC.notice() 
-				+ "최소: " + min + ' '
-				+ "비율: " + rate + ' '
-				+ "burst: " + burst + ' ',
-				null, null, 4, 3);
-				return false;
+
 		}
 		this.getTeamMark = function(num){						// 팀 상태(숫자>문자열)
 			switch(num){
